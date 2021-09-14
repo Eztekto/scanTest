d6b60161aaa9ea01e0e66a35ec23b568b7901617
Eztekto <iztekto@gmail.com>
2021-09-14 20:32:54 +0300

    Реализован вызов тестовых текстовых данных

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,16 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_scantest_MainActivity_stringFromJNI(
+Java_com_example_scantest_MainActivity_getphoneSerial(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "IT IS ALIVE";
+    std::string hello = "Phone Serial: XLJADFLJADHF:HSD";
     return env->NewStringUTF(hello.c_str());
+}
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_scantest_MainActivity_getphonePackages(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string packagesString = "Phone packages: 1\n 2\n 3\n 4";
+return env->NewStringUTF(packagesString.c_str());
 }
--- app/src/main/java/com/example/scantest/MainActivity.java
+++ app/src/main/java/com/example/scantest/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.scantest.databinding.ActivityMainBinding;
 
@@ -25,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("MainActivity", "getSerial pressed");
-
+                TextView tv = binding.sampleText;
+                tv.setText(getphoneSerial());
             }
         });
         getPackages = binding.getPackagesbtn;
@@ -33,16 +35,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("MainActivity", "getPackages pressed");
-
+                TextView tv = binding.sampleText;
+                tv.setText(getphonePackages());
             }
-
         });
 
 
 
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+
     }
 
     // Used to load the 'scantest' library on application startup.
@@ -58,5 +59,13 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'scantest' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String getphoneSerial();
+    public native String getphonePackages();
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState (savedInstanceState);
+        Log.d("SYSTEM INFO", "Метода onSaveInstanceState() запущен");
+    }
 }
